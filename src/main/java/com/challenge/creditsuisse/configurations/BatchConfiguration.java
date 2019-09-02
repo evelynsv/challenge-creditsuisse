88a7c26c8dc6ede4c769366a061b4973801d4fa4
Evelyn Vieira <vieira.evelyn05@gmail.com>
2019-09-02 19:40:12 +0100

    Batch configuration and json mapper

---
+++ src/main/java/com/challenge/creditsuisse/configurations/BatchConfiguration.java
@@ -0,0 +1,121 @@
+
+package com.challenge.creditsuisse.configurations;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.JsonLineMapper;
+import org.springframework.batch.item.file.separator.JsonRecordSeparatorPolicy;
+import org.springframework.batch.item.file.separator.RecordSeparatorPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import com.challenge.creditsuisse.batch.EventItemProcessor;
+import com.challenge.creditsuisse.batch.EventItemWriter;
+import com.challenge.creditsuisse.batch.JobCompletionListener;
+import com.challenge.creditsuisse.batch.StepParameters;
+import com.challenge.creditsuisse.canonical.Event;
+import com.challenge.creditsuisse.entities.EventDetails;
+
+/**
+ * @author evelynvieira
+ */
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Autowired
+	private StepParameters stepParameters;
+
+	@Autowired
+	private JobCompletionListener jobListener;
+
+	@Autowired
+	private JobRepository jobRepository;
+
+	@Bean
+	@Qualifier("eventJobLauncher")
+	public JobLauncher eventJobLauncher() throws Exception {
+		final SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
+		simpleJobLauncher.setJobRepository(jobRepository);
+		simpleJobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor("Event."));
+		return simpleJobLauncher;
+	}
+
+	@Bean
+	public Job job() throws Exception {
+		return jobBuilderFactory.get("job")
+				.incrementer(new RunIdIncrementer())
+				.repository(jobRepository)
+				.listener(jobListener)
+				.flow(step())
+				.end()
+				.build();
+	}
+
+	@Bean
+	public Step step() throws Exception {
+		return stepBuilderFactory.get("step")
+				.<Event, EventDetails>chunk(10)
+				.reader(eventItemReader())
+				.processor(processor())
+				.writer(writer())
+				.build();
+	}
+
+	@Bean
+	@StepScope
+	public FlatFileItemReader<Event> eventItemReader() throws Exception {
+		final FlatFileItemReader<Event> reader = new FlatFileItemReader<Event>();
+		reader.setResource(new FileSystemResource(stepParameters.getFilePath()));
+		reader.setRecordSeparatorPolicy(eventRecordSeparatorPolicy());
+		reader.setLineMapper(eventLineMapper());
+		return reader;
+	}
+
+	@Bean
+	public RecordSeparatorPolicy eventRecordSeparatorPolicy() {
+		return new JsonRecordSeparatorPolicy();
+	}
+
+	@Bean
+	public LineMapper<Event> eventLineMapper() {
+		final WrappedJsonLineMapper lineMapper = new WrappedJsonLineMapper();
+		lineMapper.setDelegate(targetEventsLineMapper());
+		return lineMapper;
+	}
+
+	@Bean
+	public JsonLineMapper targetEventsLineMapper() {
+		return new JsonLineMapper();
+	}
+
+	@Bean
+	public EventItemProcessor processor() {
+		return new EventItemProcessor();
+	}
+
+	@Bean
+	public EventItemWriter writer() {
+		return new EventItemWriter();
+	}
+}
---
+++ src/main/java/com/challenge/creditsuisse/configurations/WrappedJsonLineMapper.java
@@ -0,0 +1,51 @@
+
+package com.challenge.creditsuisse.configurations;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.JsonLineMapper;
+
+import com.challenge.creditsuisse.canonical.Event;
+
+/**
+ * @author evelynvieira
+ */
+public class WrappedJsonLineMapper implements LineMapper<Event> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(WrappedJsonLineMapper.class);
+
+	public static final String ID = "id";
+	public static final String STATE = "state";
+	public static final String TYPE = "type";
+	public static final String HOST = "host";
+	public static final String TIMESTAMP = "timestamp";
+
+	private JsonLineMapper delegate;
+
+	@Override
+	public Event mapLine(final String line, final int lineNumber) throws Exception {
+
+		final Map<String, Object> eventAsMap = delegate.mapLine(line, lineNumber);
+
+		final Event event = new Event();
+		event.setId((String) eventAsMap.get(ID));
+		event.setState((String) eventAsMap.get(STATE));
+		event.setType((String) eventAsMap.get(TYPE));
+		event.setHost((String) eventAsMap.get(HOST));
+		event.setTimestamp(Long.parseLong(eventAsMap.get(TIMESTAMP).toString()));
+
+		return event;
+	}
+
+	public JsonLineMapper getDelegate() {
+		return delegate;
+	}
+
+	public void setDelegate(final JsonLineMapper delegate) {
+		this.delegate = delegate;
+	}
+
+}
